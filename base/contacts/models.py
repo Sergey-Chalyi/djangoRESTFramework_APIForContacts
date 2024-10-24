0f5c4c2da19be182ff8692908702ac7cd780be01
Sergey_Chalyi <supersupergray@gmail.com>
2024-10-24 06:39:59 +0300

    ADDED model in app 'contacts' and made migrations

--- base/contacts/models.py
+++ base/contacts/models.py
@@ -1,3 +1,19 @@
+import re
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+
+def validate_phone_number(self, phone_number):
+    if not re.match(r'^\+\d+$', phone_number):
+        raise ValidationError('Phone number must start with + and contain only numbers.')
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=255, null=False, blank=False)
+    last_name = models.CharField(max_length=255, null=False, blank=False)
+    phone_number = models.CharField(max_length=255, null=False, blank=False, unique=True, validators=[validate_phone_number])
+    email = models.EmailField(max_length=255, null=False, blank=False, unique=True)
+
+    def __str__(self):
+        return f'{self.first_name}_{self.last_name}'
+
